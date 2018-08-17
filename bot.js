6e1f4af7ad5ae3351a55432149ca703361a6847d
rafi1010 <41636364+rafi1010@users.noreply.github.com>
2018-08-17 04:20:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,9 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.login(process.env.BOT_TOKEN);
-
-var Games = [
-        "Sevdiğiyle Uyuyor... 🌙"
-    ];
-
 client.on('ready', () => {
     console.log(`Logeada!`);
-    client.user.setGame(Games, "https://www.twitch.tv/antiquary01");
+    client.user.setGame("Sevdiğiyle Uyuyor... 🌙", "https://www.twitch.tv/antiquary01");
+    
+  
   });
