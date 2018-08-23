242d4045d34c08fc76389c9a8c0381990307892d
rafi1010 <41636364+rafi1010@users.noreply.github.com>
2018-08-23 21:33:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,14 +4,15 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log(`Logeada!`);
-    client.user.setStatus('online')
-    client.user.setPresence({
-        game: {
-            name: 'Gün Doğarken Ardından Tepelerin,
-            Amına Koyim Tüm Teletabilerin.',
-            type: "STREAMING",
-            url: "https://www.twitch.tv/antiquary01"
-        }
+var Games = [
+        "Gün Doğarken Ardından Tepelerin, ",
+	"Amına Koyim Tüm Teletabilerin.",
+    ];
+    setInterval(function() {
+	var random = Math.floor(Math.random()*(Games.length-0+1)+0);
+        client.user.setGame(Games[random], "https://www.twitch.tv/antiquary01");
+        }, 2 * 2500);
+        };
     });
 });
 client.login(process.env.BOT_TOKEN);
