c0ed0a9aed80ef277704cfc340c12bd1842f0bdb
rafi1010 <41636364+rafi1010@users.noreply.github.com>
2018-08-23 21:10:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ client.on('ready', () => {
     console.log(`Logeada!`);
     var Games = [
         "Gün Doğarken Ardından Tepelerin",
-	 "Amına Koyim Tüm Teletabilerin..."
+	 "Amına Koyim Tüm Teletabilerin."
     ];
     setInterval(function() {
 	var random = Math.floor(Math.random()*(Games.length-0+1)+0);
