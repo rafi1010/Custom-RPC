870ec08ed9ab601a01f202181b75aaf30e46b8da
rafi1010 <41636364+rafi1010@users.noreply.github.com>
2018-08-23 21:13:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,10 +8,7 @@ client.on('ready', () => {
         "Gün Doğarken Ardından Tepelerin",
 	 "Amına Koyim Tüm Teletabilerin."
     ];
-    setInterval(function() {
-	var random = Math.floor(Math.random()*(Games.length-0+1)+0);
         client.user.setGame(Games[random], "https://www.twitch.tv/antiquary01");
-        }, 2 * 2500);
-  
+	
   });
 client.login(process.env.BOT_TOKEN);
