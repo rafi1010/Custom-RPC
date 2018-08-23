511488516754115775e43de37a9e0876e8af906c
rafi1010 <41636364+rafi1010@users.noreply.github.com>
2018-08-23 21:41:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,16 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-
-
+client.login(process.env.BOT_TOKEN);
 client.on('ready', () => {
     console.log(`Logeada!`);
 var Games = [
         "Gün Doğarken Ardından Tepelerin, ",
-	"Amına Koyim Tüm Teletabilerin.",
+	"Amına Koyayım Tüm Teletabilerin...",
     ];
     setInterval(function() {
 	var random = Math.floor(Math.random()*(Games.length-0+1)+0);
         client.user.setGame(Games[random], "https://www.twitch.tv/antiquary01");
         }, 2 * 2500);
-    });
-});
-client.login(process.env.BOT_TOKEN);
+    
+  
+  });
