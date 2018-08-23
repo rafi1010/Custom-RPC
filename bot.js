e7a062951061ff92f2c9cb42d91429cfa4b09560
rafi1010 <41636364+rafi1010@users.noreply.github.com>
2018-08-23 21:35:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ var Games = [
 	var random = Math.floor(Math.random()*(Games.length-0+1)+0);
         client.user.setGame(Games[random], "https://www.twitch.tv/antiquary01");
         }, 2 * 2500);
-        };
+        });
     });
 });
 client.login(process.env.BOT_TOKEN);
